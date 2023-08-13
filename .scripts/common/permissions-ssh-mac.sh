43319548fd1227f8ce4f4009cf5353c14dd5bf22
Igor Leonovich <8408481+igorleonovich@users.noreply.github.com>
2023-08-13 04:05:11 +0400

    permissions-ssh: Add root

--- .scripts/common/permissions-ssh-mac.sh
+++ .scripts/common/permissions-ssh-mac.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-SSH_PATH=$HOME/.ssh
-chmod 700 $SSH_PATH
-for file in "`find $SSH_PATH -type f | grep -vE "\.pub$"`"; do chmod 600 $file; done
-for file in "`find $SSH_PATH -type f | grep -E "\.pub$"`"; do chmod 644 $file; done
+USER_SSH_PATH=$HOME/.ssh
+ROOT_SSH_PATH=/var/root/.ssh
+SSH_PATHS=( $USER_SSH_PATH $ROOT_SSH_PATH )
+for SSH_PATH in "${SSH_PATHS[@]}"
+do
+  chmod 700 $SSH_PATH
+  for file in "`find $SSH_PATH -type f | grep -vE "\.pub$"`"; do chmod 600 $file; done
+  for file in "`find $SSH_PATH -type f | grep -E "\.pub$"`"; do chmod 644 $file; done
+done
